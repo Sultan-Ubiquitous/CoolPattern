1daf2f999ed3084ab070e87ebfd470f2df53583b
Azim <122721166+Sultan-Ubiquitous@users.noreply.github.com>
2023-12-12 02:29:50 +0530

    can draw using circles now

--- logik.js
+++ logik.js
@@ -1,35 +1,49 @@
 const canvas = document.getElementById('LienzoDos');
 const ctx = canvas.getContext('2d');
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+//setting up the mood
 
 const mouse = {
  x: null,
  y: null,
 };
 
-window.addEventListener('click', function(event){
-    mouse.x=event.x;
-    mouse.y=event.y;
-    console.log(event);
-});
-
-window.addEventListener('resize', function(){
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    ctx.fillStyle = 'white';
-    ctx.fillRect(30,30,200,120);
-    
-    ctx.beginPath();
-    ctx.arc(500,500, 100, 0, Math.PI*2);
-    ctx.fill();
-});
-
+let isMouseDown = false;
 
 function drawCircle(){
     ctx.fillStyle = 'white';
     ctx.beginPath();
-    ctx.arc(mouse.x,mouse.y, 100, 0, Math.PI*2);
+    ctx.arc(mouse.x,mouse.y, 50, 0, Math.PI*2);
     ctx.fill();
 }
+
+window.addEventListener('resize', function(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
+
+//All functions and stuff to draw starts here
+window.addEventListener('mousedown', function(){
+    isMouseDown = true;
+});
+window.addEventListener('mouseup', function(){
+    isMouseDown = false;
+});
+
+window.addEventListener('click', function(event){
+    mouse.x = event.x;
+    mouse.y = event.y;
+    drawCircle();
+})
+
+window.addEventListener('mousemove', function(event){
+    if(isMouseDown){
+        mouse.x = event.x;
+        mouse.y = event.y;
+        drawCircle();
+    }
+});
+
+
